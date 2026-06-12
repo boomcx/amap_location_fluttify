58a0b6cff81501c86904f952ad1ff428f018c071
hao <707939932@qq.com>
2026-06-12 10:37:57 +0800

    - 暂存

--- lib/amap_location_fluttify.dart
+++ lib/amap_location_fluttify.dart
@@ -3,3 +3,5 @@ library amap_location_fluttify;
 export 'src/facade/amap_location.dart';
 export 'src/facade/enums.dart';
 export 'src/facade/models.dart';
+export 'src/ios/ios.export.g.dart';
+export 'src/android/android.export.g.dart';
